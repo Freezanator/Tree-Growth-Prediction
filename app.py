3b114a06f31254059bf287960fed3bf67ff23407
Warren Carvalho <freezanator95@yahoo.com>
2024-01-02 23:52:53 +0800

    Updated app.py

--- app.py
+++ app.py
@@ -2,24 +2,18 @@ import streamlit as st
 import pandas as pd
 
 # Reads from the dataset
-df = pd.read_csv('Results Data Pasoh.csv')
+df = pd.read_csv('Data Pasoh Results.csv.csv')
 
 # Title
 st.title('Pasoh DBH Prediction')
 
-# Dropdown for selecting SP
-selected_sp = st.selectbox('Select SP:', df['SP'].unique())
-
-# Filter DataFrame based on selected SP
-filtered_df = df[df['SP'] == selected_sp]
-
 # Dropdown for selecting TAG
-selected_tag = st.selectbox('Select TAG:', filtered_df['TAG'])
+selected_tag = st.selectbox('Select TAG:', df['TAG'])
 
 # Display selected row data horizontally
-selected_row = filtered_df[filtered_df['TAG'] == selected_tag].squeeze()
+selected_row = df[df['TAG'] == selected_tag].squeeze()
 st.write('Selected Row Data:')
-st.write(pd.DataFrame(selected_row[['TAG', 'QUAD', 'XCO', 'YCO', 'D12', 'D15', 'D17', 'D19', 'D21', 'D23 (Predicted)']]).transpose())
+st.write(pd.DataFrame(selected_row[['TAG', 'QUAD', 'XCO', 'YCO', 'DBH2017', 'DBH2019', 'DBH2021', 'DBH2023']]).transpose())
 
 # Plot graph for selected TAG using st.line_chart
-st.line_chart(selected_row[['D12', 'D15', 'D17', 'D19', 'D21', 'D23 (Predicted)']])
+st.line_chart(selected_row[['DBH2017', 'DBH2019', 'DBH2021', 'DBH2023']])
