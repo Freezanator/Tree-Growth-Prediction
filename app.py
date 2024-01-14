22afea5545d5b6a8e45100c8d3e94a0de067a0a0
Warren Carvalho <freezanator95@yahoo.com>
2024-01-14 18:29:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,6 @@ import pandas as pd
 import folium
 from st_pages import Page, show_pages, add_page_title
 
-add_page_title()
-
-show_pages(
-	[
-		Page("app.py", "Home")
-	]
-)
-
 def main():
     st.title("Interactive Map with Streamlit and Folium")
     st.header("Pasoh Reserve Forest Location")
