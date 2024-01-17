1359cfa28e4bd794ab69c4cafd1cc5cfb6067fe5
Warren Carvalho <freezanator95@yahoo.com>
2024-01-17 19:43:27 +0800

    Update 2_📊_Statistics.py

--- pages/2_📊_Statistics.py
+++ pages/2_📊_Statistics.py
@@ -3,6 +3,13 @@ import streamlit as st
 def main():
 	st.header('Statistics')
 
+col1, col2 = st.columns(2)
+    
+with col1:
+    st.metric(label="Average growth of 2019 to 2021 (Actual)", value="+0.57 cm", delta="+0.07 cm")
+
+with col2:
+    st.metric(label="Average growth of 2021 to 2023 (Predicted)", value="+0.58 cm", delta="+0.01 cm")
 
 if __name__ == "__main__":
     main()
