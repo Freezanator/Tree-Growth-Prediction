17bc64872cd76ea22a0977a2d749d5dff005f8a6
Warren Carvalho <freezanator95@yahoo.com>
2024-01-17 20:21:21 +0800

    Update 2_📊_Statistics.py

--- pages/2_📊_Statistics.py
+++ pages/2_📊_Statistics.py
@@ -20,7 +20,7 @@ def main():
     # Create a dataframe from the list of lists
     df = pd.DataFrame({
         "DBH Class": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-        "DBH Range": ["5.1-10 cm", "10.1-20 cm","20.1-30 cm","30.1-40 cm","40.1-50 cm","50.1-60 cm","60.1-70 cm",">70 cm"]
+        "DBH Range": ["<5cm", "5.1-10 cm", "10.1-20 cm","20.1-30 cm","30.1-40 cm","40.1-50 cm","50.1-60 cm","60.1-70 cm",">70 cm"]
     })
     st.dataframe(df, hide_index="True")
 
