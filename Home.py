d0a793217882ee2181ad4d67cf7b942897e25730
Warren Carvalho <freezanator95@yahoo.com>
2024-01-17 12:27:01 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,8 +9,8 @@ add_page_title()
 show_pages(
     [
         Page("Home.py", "Home"),
-        Page("pages/Statistics.py", "Statistics"),
-        Page("pages/DigitalTwin.py", "Digital Twin"),
+        Page("pages/1_Statistics.py", "Statistics"),
+        Page("pages/2_DigitalTwin.py", "Digital Twin"),
     ]
 )
 
@@ -37,7 +37,6 @@ def map(df):
 
 
 def main():
-    primaryColor = "#2b3931"
     st.header("Introduction")
 
     st.write("The Pasoh Forest Reserve is a nature reserve situated approximately 8 km from Simpang Pertang, Malaysia, and about 70 km southeast of Kuala Lumpur. It spans an area of 2,450 hectares, with a central zone of 600 hectares encircled by a buffer zone. The reserve is bordered by palm oil plantations on three sides, while a selectively logged dipterocarp forest adjoins the remaining side. The reserve receives an annual rainfall averaging 2 metres, with a range between 1,728 and 3,112 mm. In 1987, a 50-hectare forest dynamics plot was set up within the reserve. This initiative was a joint effort between the Forest Research Institute Malaysia, the Forest Global Earth Observatory (previously known as the Center for Tropical Forest Science), and the Smithsonian Tropical Research Institute. Since 1989, three censuses have been conducted in the plot, recording approximately 340,000 trees from 800 different species.")
