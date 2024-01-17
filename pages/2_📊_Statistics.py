b27a910a570ea39796b7092755dee0a6111de65a
Warren Carvalho <freezanator95@yahoo.com>
2024-01-17 19:45:21 +0800

    Update 2_📊_Statistics.py

--- pages/2_📊_Statistics.py
+++ pages/2_📊_Statistics.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 def main():
-	st.header('Statistics')
+    st.header('Statistics')
 
     col1, col2 = st.columns(2)
-        
+
     with col1:
         st.metric(label="Average growth of 2019 to 2021 (Actual)", value="+0.57 cm", delta="+0.07 cm")
 
