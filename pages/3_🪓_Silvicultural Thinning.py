fb5123c4a3668641727647ce4bbbeddbf9070922
Warren Carvalho <freezanator95@yahoo.com>
2024-01-31 21:18:32 +0800

    Update 3_🪓_Silvicultural Thinning.py

--- pages/3_🪓_Silvicultural Thinning.py
+++ pages/3_🪓_Silvicultural Thinning.py
@@ -8,7 +8,13 @@ def map(df2):
     # Convert 'SP' column to categorical
     df2['SPECIES'] = df2['SPECIES'].astype('category')
 
-    fig = px.scatter(df2, x='XCO', y='YCO', color='SPECIES', color_discrete_sequence=px.colors.qualitative.Pastel, labels={'SPECIES':'Species'}, hover_data={'DBH2021':'DBH', 'GROWTH1921':'Growth', 'CLASS2021':'DBH Class'})
+    # Add custom hover data
+    hover_data = {
+        'Growth': 'Growth Rate',
+        'DBH': 'Diameter at Breast Height'
+    }
+    
+    fig = px.scatter(df2, x='XCO', y='YCO', color='SPECIES', color_discrete_sequence=px.colors.qualitative.Pastel, labels={'SPECIES':'Species'}, hover_data=hover_data)
 
     # Update title attributes
     fig.update_layout(
