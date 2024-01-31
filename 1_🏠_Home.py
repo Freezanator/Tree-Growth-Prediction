fdea0053360d81b026d94b08d2ca16a570a2fc76
Warren Carvalho <freezanator95@yahoo.com>
2024-01-31 16:03:00 +0800

    Update app

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -9,6 +9,7 @@ def folium_static_map(m):
     html = m.get_root().render()
     st.components.v1.html(html, width=width, height=height)
 
+
 def map(df):
     # Convert 'SP' column to categorical
     df['SP'] = df['SP'].astype('category')
@@ -24,18 +25,6 @@ def map(df):
 
     st.plotly_chart(fig)
 
-def set_sidebar_background(image_path):
-    st.markdown(
-        f"""
-        <style>
-        .sidebar .sidebar-content {{
-            background-image: url({image_path});
-            background-size: cover;
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
 
 def main():
     st.header("Introduction")
@@ -90,9 +79,6 @@ def main():
     else:
         # Show a plot of all species
         map(df)
-
-    # Set the sidebar background
-    set_sidebar_background('Slides 1-1.png')
 
 
 if __name__ == "__main__":
--- pages/2_📊_Statistics.py
+++ pages/2_📊_Statistics.py
@@ -74,6 +74,25 @@ def main():
             # Create a bar chart using st.bar_chart()
             st.subheader(f"Average DBH of trees in DBH Class {col}")
             st.bar_chart(df[["Year", col]].set_index("Year"))
+    
+    st.divider()
+
+    # Read the csv files
+    df_2013 = pd.read_csv('DBH2013.csv')
+    df_2015 = pd.read_csv('DBH2015.csv')
+
+    # Create a tab selector
+    tab = st.sidebar.selectbox('Select a year', ['2013', '2015'])
+
+    # Display the selected tab
+    st.title(f'DBH Class Distribution in {tab}')
+
+    # Create a bar chart based on the selected tab
+    if tab == '2013':
+        st.bar_chart(df_2013.set_index('DBH Class')) # Use streamlit's bar_chart and set the index to DBH Class
+    else:
+        st.bar_chart(df_2015.set_index('DBH Class')) # Use streamlit's bar_chart and set the index to DBH Class
+
 
 
 
