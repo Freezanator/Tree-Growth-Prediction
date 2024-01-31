8bb8488eca17a1d9e353e71ab4eac40c68c69d07
Warren Carvalho <freezanator95@yahoo.com>
2024-01-31 16:27:22 +0800

    Update app

---
+++ pages/3_🪓_Silvicultural Thinning.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.header("Silvicultural Thinning")
+
+    st.write("Thinning is a silvicultural operation where the main objective is to reduce the density of trees in a stand, improve the quality and growth of the remaining trees and produce a saleable product. Thinning can also achieve other objectives such as altering the species composition of a stand, improving the health of the remaining trees or disturbing an established ground flora to enhance opportunities for natural regeneration.")
+
+    st.divider()
+
+    
